3307ebd3c0ceec14665b227223a6a1dcb6e06167
Mariperu <maritzarg2020@gmail.com>
2021-10-18 18:59:32 -0500

    Resolv conf

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
-// import App from './components/App.jsx';
-// import Home from './components/home.jsx';
-// import Waiter from './components/waiter.jsx';
-// import Waiter2 from './components/waiter2.jsx';
-import Waiter3 from './components/waiter3.jsx';
-
-ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    {/* <Home /> */}
-    {/* <Waiter /> */}
-    {/* <Waiter2 /> */}
-    <Waiter3 />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-=======
 import App from './components/App.jsx';
 
 
@@ -33,5 +14,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
->>>>>>> df9dbe9efb699e291342517bd12c811c54bb759c
 
